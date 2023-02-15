bec0c77e2cba6fb5fbcc551df1dbc0cc89a06721
Adnan Rizvanovik <Adnan Rizvanovik@DESKTOP-0DUNRFC>
2023-02-15 10:57:51 +0100

    Projektdateien hinzufügen.

---
+++ Rest_Server_Image_Manager/Controllers/image.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Rest_Server_Image_Manager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class image : ControllerBase
+    {
+       
+        
+        [HttpGet("gettingimage")]
+        public async Task<string> GettingzutImage2()
+        {
+
+            return "test";
+        }
+        [HttpGet("getting_status")]
+        public async Task<string> getting_status()
+        {
+            Task.Delay(10);
+            return "server is running";
+        }
+        [HttpGet("power_off_server")]
+        public async Task<string> shutting_off()
+        {
+            Task.Delay(10);
+            Environment.Exit(1);
+            return "server is shutting down";
+        }
+        [HttpPost("imagepost")]
+        public async Task<IActionResult> gettingimage()
+        {
+            IFormFile file = Request.Form.Files.FirstOrDefault();
+            var orignialFileName = Path.GetFileName(file.Name);
+            var uniqueFileName = Path.GetRandomFileName()+".png";
+            var uniqueFilePath = Path.Combine(@"C:\Users\Adnan Rizvanovik\Desktop\images", uniqueFileName);
+            using (var stream = System.IO.File.Create(uniqueFilePath))
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            return Ok("File has been uploaded to server");
+        }
+    }
+}
+ 
---
+++ olpadeojdemtest/Program.cs
@@ -0,0 +1,11 @@
+﻿
+Uri baseUri = new Uri("http://localhost:5250/api/Image/");
+HttpClient client = new HttpClient();
+client.BaseAddress = baseUri;
+
+HttpResponseMessage response = await client.GetAsync("GettingImage");
+if (!response.IsSuccessStatusCode)
+    throw new InvalidOperationException();
+string content = await response.Content.ReadAsStringAsync();
+
+Console.ReadKey();
